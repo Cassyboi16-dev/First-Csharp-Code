0745543befac54a55221b51cf381c57f3adc56ec
Samuel <samuelosi1636@gmail.com>
2026-09-16 18:19:21 +0100

    First Csharp Code

---
+++ main.cs
@@ -0,0 +1,31 @@
+// Console.WriteLine("Hello Samuel");
+
+Console.WriteLine("Congrats,");
+Console.Write(" ");
+Console.Write("You did it well done");
+
+
+Console.Write("Yoo Yoo ");
+Console.WriteLine("It's me again");
+
+Console.WriteLine("Just checking");
+
+Console.WriteLine(123 + " Integers" + " & Whole Numbers");
+Console.WriteLine(0.24 + " Double");
+Console.WriteLine(2.56m + " Decimal");
+Console.WriteLine(67.23f + " float");
+
+Console.WriteLine(true);
+Console.WriteLine(false);
+
+string name;
+
+char gameModes;
+
+int player1;
+
+decimal pingTime;
+
+float refreshTime;
+
+bool isOnline;
