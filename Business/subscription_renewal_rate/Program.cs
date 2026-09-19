819e7eed79aee7e59fdb3cc440173a5fe1fddfec
Samuel <samuelosi1636@gmail.com>
2026-09-19 16:44:42 +0100

    Added more exercises

---
+++ Business/subscription_renewal_rate/Program.cs
@@ -0,0 +1,22 @@
+﻿Random random = new Random();
+
+int dayUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+
+ if (dayUntilExpiration == 0)
+{
+    Console.WriteLine("Your subscription has expired.");
+}
+else if (dayUntilExpiration == 1)
+{
+    Console.WriteLine($"Your subscription expires within a day!\nRenew now and save {discountPercentage += 20}%!");
+}
+else if (dayUntilExpiration <= 5)
+{
+    Console.WriteLine($"Your subscription expires in {dayUntilExpiration} days.\nRenew now and save {discountPercentage += 10}%");
+}   
+if(dayUntilExpiration <= 10)
+{
+    Console.WriteLine("Your subscription will expire soon, Renew now!");
+}
---
+++ Dice_Game/logic/Program.cs
@@ -0,0 +1,66 @@
+﻿
+// initializing a new random method
+
+Random dice = new Random();
+
+
+// adding it to the Next() method for adding the range of random values
+
+int roll1 = dice.Next(1,7);
+int roll2 = dice.Next(1,7);
+int roll3 = dice.Next(1,7);
+
+
+
+
+// roll1 = 5;
+// roll2 = 1;
+// roll3 = 1;
+
+
+
+// sum of the rest from the Next() method
+
+int total = roll1 + roll2 + roll3;
+
+// total = 7;
+
+
+// Output of the values and total sum
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+// if statements for the logic && bonus points
+
+if((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+{
+    if((roll1 == roll2) && (roll2 == roll3))
+    {
+        Console.WriteLine("You rolled triples! +6 bonus to total!");
+        total += 6;
+    }
+    else
+    {
+        Console.WriteLine("You rolled doubles! +2 bonus to total");
+        total += 2;
+    }
+    Console.WriteLine($"Your total including the bonus: {total}");
+}
+
+
+if(total >= 1)
+{
+    Console.WriteLine("You win a new car!");
+}else if (total >= 10)
+{
+    Console.WriteLine("You win a laptop!");
+}else if (total == 7)
+{
+    Console.WriteLine("You win a trip for two!");
+}
+
+else
+{
+    Console.WriteLine("Sorry, you lose you get nothing.");
+   
+}
